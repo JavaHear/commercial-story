8ea39fdc2435efbf4423a448f049ad2df63e85e7
javokhir-tursunboev <jtursunboev@bk.ru>
2022-02-13 16:39:55 +0900

    this is my commit

---
+++ category/models.py
@@ -0,0 +1,19 @@
+from distutils.command.upload import upload
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=200 , blank= True)
+    cat_image = models.ImageField(upload_to = 'photos/categrories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug ])
+
+    def __str__(self) -> str:
+        return self.category_name
